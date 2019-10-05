73735efb489e9aea2d6d42646d8c9d5d7cd59bfc
phil-jonesQ <phil.jones.24.4@gmail.com>
2019-10-05 23:08:05 +0100

    Change Scoring system to show player 1 and player 2 results and win or draw

--- game_of_dice.py
+++ game_of_dice.py
@@ -17,8 +17,8 @@ start_y = WindowHeight / 2 - scale - offset
 dice_width = 225
 dice_length = 225
 # Use a dictionary to store the results
-result = {}
-player = 1
+result_p1 = {}
+result_p2 = {}
 round = 0
 
 BLACK = (0, 0, 0)
@@ -70,59 +70,99 @@ def throw():
     return dice_throw
 
 
-def update_score(player, round, current_throw):
-    d = {"Player " + str(player) + " for round " + str(round): current_throw}
-    result.update(dict(d))  # update it
-    #print (result)
+def update_score(round, go, current_throw):
+    if go == 1:
+        d = {"Player 1 for round " + str(round): current_throw}
+        result_p1.update(dict(d))  # update it
+    else:
+        d2 = {"Player 2 for round " + str(round): current_throw}
+        result_p2.update(dict(d2))
+    #print(result_p1)
+    #print(result_p2)
 
 
-def draw_dice(surface, throw):
+def draw_dice(surface, throw, go):
     # Wipe Screen
     dice_surface.fill(BLACK)
     # Draw Dice Border
-    pygame.draw.line(surface, RED, (start_x - offset, start_y - offset), (start_x + dice_width, start_y - offset))
-    pygame.draw.line(surface, RED, (start_x - offset, start_y - offset), (start_x - offset, start_y + dice_length))
-    pygame.draw.line(surface, RED, (start_x + dice_width, start_y - offset),
+    if go == 1:
+        my_colour = GREEN
+    else:
+        my_colour = BLUE
+    pygame.draw.line(surface, my_colour, (start_x - offset, start_y - offset), (start_x + dice_width, start_y - offset))
+    pygame.draw.line(surface, my_colour, (start_x - offset, start_y - offset), (start_x - offset, start_y + dice_length))
+    pygame.draw.line(surface, my_colour, (start_x + dice_width, start_y - offset),
                      (start_x + dice_width, start_y + dice_length))
-    pygame.draw.line(surface, RED, (start_x - offset, start_y + dice_length),
+    pygame.draw.line(surface, my_colour, (start_x - offset, start_y + dice_length),
                      (start_x + dice_width, start_y + dice_length))
     # Draw Dice Face Based on which 3x3 array has been selected
     for i in (range(3)):
         for j in (range(3)):
             # Nested loop is needed to iterate over the selected dice face array (3 x 3)
             if dice_faces[throw - 1][j][i] == "X":
-                pygame.draw.circle(surface, RED, (int(start_x + scale * i), int(start_y + scale * j)), int(scale / 3))
+                pygame.draw.circle(surface, my_colour, (int(start_x + scale * i), int(start_y + scale * j)), int(scale / 3))
     # Update the screen
     pygame.display.flip()
 
 
-def draw_score(surface, font, current_throw, font2):
-    text = font.render(str(current_throw), True, WHITE)
-    surface.blit(text, [start_x - offset, WindowHeight - offset * 2])
+def draw_round_message(surface, font, round):
+    # Wipe Screen
+    dice_surface.fill(BLACK)
+    text = font.render("SPACE TO THROW... ", True, RED)
+    surface.blit(text, [offset, WindowHeight / 2 - offset])
+    pygame.display.flip()
+
+
+def draw_score(surface, font, current_throw, font2, go):
     # Get historical scores
     offset_px = 5
-    # Print the last 10 values from our result dictionary
-    if len(result) > 1:
-        for x in list(reversed(list(result)))[1:15]:
-            history = font2.render(format(result[x]), True, GREY)
+    # Print the last 15 values from our p1 result dictionary
+    if len(result_p1) > 0:
+        for x in list(reversed(list(result_p1)))[0:15]:
+            history = font2.render(format(result_p1[x]), True, GREEN)
             surface.blit(history, [start_x + offset_px, WindowHeight - offset * 2])
             offset_px += 15
+
+    offset_px = 5
+    #print(go)
+    if len(result_p2) > 0:
+        for x in list(reversed(list(result_p2)))[0:15]:
+            history = font2.render(format(result_p2[x]), True, BLUE)
+            surface.blit(history, [start_x + offset_px, WindowHeight - offset ^ 2 * 2])
+            offset_px += 15
+    # Extract the last winner
+    for x in list(reversed(list(result_p1)))[0:1]:
+        p1_result = format(result_p1[x])
+        print(p1_result)
+    for x in list(reversed(list(result_p2)))[0:1]:
+        p2_result = format(result_p2[x])
+        print(p2_result)
+    if p1_result > p2_result:
+        text = font.render("P1", True, WHITE)
+    if p1_result < p2_result:
+        text = font.render("P2", True, WHITE)
+    if p1_result == p2_result:
+        text = font.render("D", True, WHITE)
+    surface.blit(text, [start_x - offset, WindowHeight - offset * 2])
     # Update the screen
     pygame.display.flip()
 
 
-def spin(surface):
+def spin(surface, go):
     # Generate a spin effect
     for i in range(200):
         for j in range(6):
-            draw_dice(surface, j)
+            draw_dice(surface, j, go)
 
 
 def main():
     loop = True
     # Declare Global Vars
 
-    round = 0
+    start = False
+    round = 1
+    go = 0
+    go_per_round = 2
 
     pygame.init()
     pygame.display.set_caption("Dice " + MY_VERSION)
@@ -141,13 +181,21 @@ def main():
         clock.tick(5)
 
         # Draw Dice
-        draw_dice(dice_surface, current_throw)
+        if start:
+            draw_dice(dice_surface, current_throw, go)
 
         # Update Score
-        update_score(player, round, current_throw)
+        if start:
+            update_score(round, go, current_throw)
 
         # Draw Score
-        draw_score(dice_surface, font, current_throw, font2)
+        if start and go == 2:
+            draw_score(dice_surface, font, current_throw, font2, go)
+
+        # Round start
+
+        if not start:
+            draw_round_message(dice_surface, font2, round)
 
         # Handle quit
         for event in pygame.event.get():
@@ -155,9 +203,15 @@ def main():
                 pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    round += 1
-                    spin(dice_surface)
+                    go += 1
+                    start = True
+                    if go > go_per_round:
+                        round += 1
+                        go = 1
+                        #start = False
+                    spin(dice_surface, go)
                     current_throw = throw()
+                    #print(go, round)
 
 
 # Call main
