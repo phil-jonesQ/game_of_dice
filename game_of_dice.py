28364b87bb187f5b3bde3cb0a1f28273ea75fdcb
phil-jonesQ <phil.jones.24.4@gmail.com>
2019-10-05 17:27:49 +0100

    Changed size

--- game_of_dice.py
+++ game_of_dice.py
@@ -7,10 +7,9 @@ import pygame
 import random
 
 # Constants
-
-WindowWidth = 500
-WindowHeight = 570
-scale = 67
+WindowWidth = 300
+WindowHeight = 300
+scale = 90
 start_x = WindowWidth / 2 - scale
 start_y = WindowHeight / 2 - scale
 dice_width = 200
@@ -28,58 +27,60 @@ clock = pygame.time.Clock()
 MY_VERSION = "1.00"
 
 # Dice Faces
-
-## One
+# One
 ON = ['...',
       '.X.',
       '...']
-## Two
+# Two
 TW = ['X..',
       '...',
       '..X']
-## Three
+# Three
 TH = ['X..',
       '.X.',
       '..X']
-## Four
+# Four
 FO = ['X.X',
       '...',
       'X.X']
 
-## Five
+# Five
 FI = ['X.X',
       '.X.',
       'X.X']
 
-## Six
+# Six
 SI = ['X.X',
       'X.X',
       'X.X']
 
+# Map each face array to a single array
 dice_faces = [ON, TW, TH, FO, FI, SI]
 
 
 def throw():
+    # Generate a number 1 - 6
     return random.randrange(1, 7)
 
 
 def draw_dice(surface, throw):
     # Wipe Screen
     dice_surface.fill(BLACK)
-    #print ("Throw is " + str(throw))
     # Draw Dice
     for i in (range(3)):
         for j in (range(3)):
+            # Nested loop is needed to iterate over the selected dice face array (3 x 3)
             if dice_faces[throw - 1][j][i] == "X":
-                pygame.draw.circle(surface, RED, (int(start_x + scale * i), int(start_y + scale * j)), 30)
-                #print(dice_faces[throw - 1][i][j])
+                pygame.draw.circle(surface, RED, (int(start_x + scale * i), int(start_y + scale * j)), int(scale / 3))
     # Update the screen
     pygame.display.flip()
 
-def spin(surface):
 
+def spin(surface):
+    # Generate a spin effect
     for j in range(6):
         draw_dice(surface, j)
+
 
 def main():
     loop = True
@@ -104,7 +105,7 @@ def main():
         # Draw Dice
         draw_dice(dice_surface, current_throw)
 
-        # Increase Level
+        # Handle quit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
