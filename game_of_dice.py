de7a5b88d68abcdd2e3b3922e9502d227a8b30e5
phil-jonesQ <phil.jones.24.4@gmail.com>
2019-10-08 18:16:19 +0100

    Update game_of_dice.py

--- game_of_dice.py
+++ game_of_dice.py
@@ -1,7 +1,9 @@
-""" Version 1.07 - Routine to display a Six Sided Dice
-The dice can be thrown and be used for simple games
-The current thrown value is shown in white, with the last 15 historical values displayed in Grey
-Phil Jones October 2019 - phil.jones.24.4@gmail.com
+""" Version 1.07 - Simple program to display a Six Sided Dice.
+The dice can be "thrown" and be used for simple games.
+The current value that has been "thrown", is shown in the bottom left,
+while the last 10 historical values are displayed along the bottom of the screen - with indicators
+as to who won i.e. GREEN for Player 1 and BLUE for Player 2. 
+Phil Jones October 2019 - phil.jones.24.4@gmail.com 
 V1.06 Added historic scores and results to UI
 V1.07 Toned down colours
 """
